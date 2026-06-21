edf11dc898be766781614c55237a7dfa557d3fcb
Kentaro Wada <www.kentaro.wada@gmail.com>
2026-06-21 20:01:25 +0900

    chore: add nix devShell and dprint config

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "acron development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems =
+        f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.go
+            pkgs.gofumpt
+            pkgs.golangci-lint
+            pkgs.dprint
+          ];
+        };
+      });
+
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
+    };
+}
